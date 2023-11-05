5f7066a1a0d49014bbc1ab68ba19ea9df0e998cb
Michael Otieno Kasuku <michaelotienokasuku@gmail.com>
2023-11-05 13:53:23 +0300

    models added

--- schoolvibe/settings.py
+++ schoolvibe/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'schoolvibe.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'uririconnect',
+        'USER': 'kasuku',
+        'PASSWORD': 'Student2023@Mmust',
+        'HOST': 'localhost',  # Set your PostgreSQL host address if it's not running on localhost
+        'PORT': '',  # Leave this empty to use the default PostgreSQL port (5432)
     }
 }
 
@@ -115,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- uririconnect/models.py
+++ uririconnect/models.py
@@ -1,3 +1,108 @@
+"""
+Module containing Django models for the database schema.
+
+This module defines the following models:
+- Roles: Represents different roles in the system.
+- Users: Represents user information, including authentication details.
+- Events: Represents events with associated details and organizers.
+- EventRegistrations: Represents user registrations for events.
+- Resources: Represents resources with titles, descriptions, and file paths.
+- Classes: Represents different classes with associated teachers.
+- Messages: Represents messages sent between users.
+- SecurityIncidents: Represents security incidents reported by users.
+- Assignments: Represents assignments with titles, descriptions, due dates, and associated teachers and classes.
+- Grades: Represents student grades for assignments.
+
+Each model corresponds to a table in the database schema and includes appropriate fields and relationships.
+
+Note: Some fields, such as user_password, are intended to store hashed passwords and should be handled securely in the application.
+"""
+
 from django.db import models
 
-# Create your models here.
+class Roles(models.Model):
+    role_id = models.AutoField(primary_key=True)
+    role_name = models.CharField(max_length=50, null=False)
+
+class Users(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    role_id = models.ForeignKey(Roles, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=50, null=False)
+    last_name = models.CharField(max_length=50, null=False)
+    email_address = models.EmailField(unique=True, null=False)
+    user_password = models.CharField(max_length=255)  # Hashed password handled in the application
+    phone_number = models.CharField(max_length=20, unique=True, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Events(models.Model):
+    event_id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100, null=False)
+    event_description = models.TextField(null=True)
+    event_date = models.DateField(null=True)
+    event_location = models.CharField(max_length=100, null=True)
+    organizer_id = models.ForeignKey(Users, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class EventRegistrations(models.Model):
+    event_reg_id = models.AutoField(primary_key=True)
+    event_id = models.ForeignKey(Events, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(Users, on_delete=models.CASCADE)
+    registration_date = models.DateField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Resources(models.Model):
+    resource_id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100, null=False)
+    resource_description = models.TextField(null=True)
+    file_path = models.CharField(max_length=255, null=True)
+    uploaded_by = models.ForeignKey(Users, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Classes(models.Model):
+    class_id = models.AutoField(primary_key=True)
+    class_name = models.CharField(max_length=50, null=False)
+    teacher_id = models.ForeignKey(Users, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Messages(models.Model):
+    message_id = models.AutoField(primary_key=True)
+    sender_id = models.ForeignKey(Users, related_name='sender_messages', on_delete=models.CASCADE)
+    receiver_id = models.ForeignKey(Users, related_name='receiver_messages', on_delete=models.CASCADE)
+    message_content = models.TextField(null=True)
+    sent_at = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class SecurityIncidents(models.Model):
+    security_id = models.AutoField(primary_key=True)
+    reporter_id = models.ForeignKey(Users, on_delete=models.CASCADE)
+    incident_details = models.TextField(null=True)
+    time_reported = models.DateTimeField(auto_now_add=True)
+    report_status = models.CharField(max_length=20, choices=[('Reported', 'Reported'), ('Under Investigation', 'Under Investigation'), ('Resolved', 'Resolved')])
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Assignments(models.Model):
+    assignment_id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100, null=False)
+    assignment_description = models.TextField(null=True)
+    due_date = models.DateField(null=True)
+    teacher_id = models.ForeignKey(Users, on_delete=models.CASCADE)
+    class_id = models.ForeignKey(Classes, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Grades(models.Model):
+    grade_id = models.AutoField(primary_key=True)
+    assignment_id = models.ForeignKey(Assignments, on_delete=models.CASCADE)
+    student_id = models.ForeignKey(Users, on_delete=models.CASCADE)
+    score = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    grading_date = models.DateField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
