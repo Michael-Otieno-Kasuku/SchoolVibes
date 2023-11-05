3c4fdd9d6a4fb3f84cd549d32a103c5a761d11b7
Michael Otieno Kasuku <michaelotienokasuku@gmail.com>
2023-11-05 20:02:56 +0300

    login page created

--- uririconnect/urls.py
+++ uririconnect/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index, name="index"),
+    path('', views.index, name="index"),
+    path('login/', views.login_view, name='login'),  # Add this line to define the login URL
 ]
--- uririconnect/views.py
+++ uririconnect/views.py
@@ -2,3 +2,19 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+from django.contrib.auth import authenticate, login
+
+def login_view(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+        user = authenticate(request, email=email, password=password)
+        if user is not None:
+            login(request, user)
+            # Redirect to a success page
+        else:
+            # Return an error message to the template
+            error_message = "Invalid email or password."
+            return render(request, 'login.html', {'error_message': error_message})
+    return render(request, 'login.html')
