5c1a0aeee86348517b3d03657f5f1dc1b945bfce
Michael Otieno Kasuku <michaelotienokasuku@gmail.com>
2023-11-05 13:58:19 +0300

    uririconnect app added

--- schoolvibe/settings.py
+++ schoolvibe/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'uririconnect.apps.UririconnectConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
