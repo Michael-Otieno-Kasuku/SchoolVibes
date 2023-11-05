cc027f51bb7c4a5ad0527deddf14b2593f8c2b07
Michael Otieno Kasuku <michaelotienokasuku@gmail.com>
2023-11-05 15:13:14 +0300

    time zone modified

--- schoolvibe/settings.py
+++ schoolvibe/settings.py
@@ -110,12 +110,11 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'  # Set the time zone to Africa/Nairobi for Kenya
 
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
